bd30bf9434c4fc1c22b21761dea67323decff6a4
marthaheredia48 <marthaherediaandrade@icloud.com>
2026-05-16 17:10:17 -0600

    Initial Commit

---
+++ Tutor/TutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorApp.swift
+//  Tutor
+//
+//  Created by Martha Heredia Andrade on 16/05/26.
+//
+
+import SwiftUI
+
+@main
+struct TutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TutorTests/TutorTests.swift
@@ -0,0 +1,19 @@
+//
+//  TutorTests.swift
+//  TutorTests
+//
+//  Created by Martha Heredia Andrade on 16/05/26.
+//
+
+import Testing
+@testable import Tutor
+
+struct TutorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
